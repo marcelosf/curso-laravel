339baa94509b1f1166ef2c1ea26f33b1cbe29384
marcelo <schneider.fei@gmail.com>
2017-02-22 12:47:44 +0000

    webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,32 @@ var webpack = require('webpapack');
 
 module.exports = {
 
-    devtool: 'source-map',
+    entry: {
+        admin: './resources/assets/js/admin.js'
+    },
+    output: {
+        path: __dirname + '/public/build',
+        filename: '[name].bundle.js',
+        publicPath: '/build/'
+    },
     plugins: [
-        new webpack.HotModuleReplacementPlugin()
-    ]
+        new webpack.ProvidePlugin({
+            'window.$': 'jquery',
+            'window.JQuery': 'jquery'
+        })
+    ],
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components)/,
+                loader: 'babel'
+            },
+            {
+                test: /\.vue$/,
+                loader: 'vue'
+            }
+        ]
+    }
 
 };
