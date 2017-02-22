88c8855ce5dfcb7addd50361c12c4b3b3149b085
marcelo <schneider.fei@gmail.com>
2017-02-22 13:03:17 +0000

    Configuração do webpack em gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,14 @@
 const elixir = require('laravel-elixir');
+const webpackConfig = require('./webpack.config');
+const webpackDevConfig = require('./webpack.dev.config');
 
 require('laravel-elixir-vue-2');
+require('laravel-elixir-webpack-official');
 
+Elixir.webpack.config.module.loaders = [];
+
+Elixir.webpack.mergeConfig(webpackConfig);
+Elixir.webpack.mergeConfig(webpackDevConfig);
 
 elixir(mix => {
     mix.sass('./resources/assets/admin/sass/admin.scss')
