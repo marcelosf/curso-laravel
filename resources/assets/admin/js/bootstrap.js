2201c67b93c63a6ff38cb84cb1f3334fa9f8e2c6
marcelo <schneider.fei@gmail.com>
2017-03-07 13:10:36 +0000

    Configurando menus administrativos

--- resources/assets/admin/js/bootstrap.js
+++ resources/assets/admin/js/bootstrap.js
@@ -7,6 +7,7 @@
 window.Vue = require('vue');
 require('materialize-css');
 
+
 /**
  * We'll load the axios HTTP library which allows us to easily issue requests
  * to our Laravel back-end. This library automatically handles sending the
