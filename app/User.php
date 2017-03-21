57f1f4a02114322509e7bd810b6b411b19c4bfdb
vagrant <vagrant@curso-laravel>
2017-03-21 16:11:26 +0000

    Criando método para criar token

--- app/User.php
+++ app/User.php
@@ -4,8 +4,9 @@ namespace CodeFin;
 
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
 
     /**
@@ -32,4 +33,29 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+
+    public function getJWTCustomClaims()
+    {
+
+        return [
+
+            'user' => [
+
+                'id' => $this->id,
+                'name' => $this->name,
+                'email' => $this->email
+
+            ]
+
+        ];
+
+    }
+
+    public function getJWTIdentifier()
+    {
+
+        return $this->id;
+
+    }
 }
