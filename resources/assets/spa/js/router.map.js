de9be2ec3b053405d50e4c997bd7cb6cb0b403f0
vagrant <vagrant@curso-laravel>
2017-03-28 16:01:50 +0000

    Organizando Rotas

---
+++ resources/assets/spa/js/router.map.js
@@ -0,0 +1,10 @@
+import LoginComponent from './components/Login.vue';
+
+export default {
+
+    '/login': {
+        name: 'auth.login',
+        component: LoginComponent
+    }
+
+}
--- resources/assets/spa/js/spa.js
+++ resources/assets/spa/js/spa.js
@@ -1,21 +1,6 @@
-import LoginComponent from './components/Login.vue';
-import AppComponent from './components/App.vue';
-
-/**
- * Vue is a modern JavaScript library for building interactive web interfaces
- * using reactive data binding and reusable components. Vue's API is clean
- * and simple, leaving you to focus on building your next great project.
- */
-
 window.Vue = require('vue');
 require('materialize-css');
 
-
-/**
- * We'll load the axios HTTP library which allows us to easily issue requests
- * to our Laravel back-end. This library automatically handles sending the
- * CSRF token as a header based on the value of the "XSRF" token cookie.
- */
 
 // window.axios = require('axios');
 //
@@ -27,31 +12,7 @@ require('materialize-css');
 Vue.component('app', require('./components/App.vue'));
 Vue.component('admin-menu', require('./components/AdminMenu.vue'));
 
-let VueRouter = require('vue-router');
-const router = new VueRouter();
-
-router.map({
-
-    '/login': {
-        name: 'auth.login',
-        component: LoginComponent
-    }
-
-});
-
-router.start({
-
-    components: {
-        'app': AppComponent
-    }
-
-}, 'body');
-
-/**
- * Echo exposes an expressive API for subscribing to channels and listening
- * for events that are broadcast by Laravel. Echo and event broadcasting
- * allows your team to easily build robust real-time web applications.
- */
+require('./router');
 
 // import Echo from "laravel-echo"
 
